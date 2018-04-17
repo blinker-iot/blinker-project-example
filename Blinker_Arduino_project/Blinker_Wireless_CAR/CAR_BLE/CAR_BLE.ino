b1b34028b1d20d9449866739cf1590691ccf1826
i3water <121024123@qq.com>
2018-04-17 18:28:19 +0800

    update wireless car example

---
+++ Blinker_Arduino_project/Blinker_Wireless_CAR/CAR_BLE/CAR_BLE.ino
@@ -0,0 +1,139 @@
+/*
+ * Blinker BLE CAR, support Arduino UNO
+ */
+
+#define BLINKER_PRINT	Serial
+#define BLINKER_BLE
+
+#include <Blinker.h>
+
+#define L_DIR_PIN       7
+#define L_PWM_PIN       6
+#define R_DIR_PIN       4
+#define R_PWM_PIN       5
+#define C_BAR_PIN       9
+
+uint32_t car_os_time = millis();
+bool isWarn = false;
+
+void blinker_car_init()
+{
+    pinMode(L_DIR_PIN, OUTPUT);
+    pinMode(L_PWM_PIN, OUTPUT);
+    pinMode(R_DIR_PIN, OUTPUT);
+    pinMode(R_PWM_PIN, OUTPUT);
+
+    pinMode(C_BAR_PIN, INPUT_PULLUP);
+}
+
+void blinker_car_parse(uint8_t &_L_PWM, bool &_L_DIR, uint8_t &_R_PWM, bool &_R_DIR)
+{
+    uint8_t xAxis = Blinker.joystick(J_Xaxis);
+    uint8_t yAxis = Blinker.joystick(J_Yaxis);
+    bool    isStop = false;
+
+    if (yAxis < 128) {
+        _L_DIR = true;
+        _R_DIR = true;
+
+        _L_PWM = map(abs(128-yAxis), 0, 128, 128, 255);
+        _R_PWM = map(abs(128-yAxis), 0, 128, 128, 255);
+    }
+    else if (yAxis > 128) {
+        _L_DIR = false;
+        _R_DIR = false;
+
+        _L_PWM = map(abs(128-yAxis), 0, 128, 128, 255);
+        _R_PWM = map(abs(128-yAxis), 0, 128, 128, 255);
+    }
+    else {
+        isStop = true;
+        
+        _L_PWM = 0;
+        _R_PWM = 0;
+    }
+
+    if (xAxis < 128) {
+        if (isStop) {
+            _L_DIR = false;
+            _R_DIR = true;
+
+            _L_PWM = map(abs(128-xAxis), 0, 128, 128, 255);
+            _R_PWM = map(abs(128-xAxis), 0, 128, 128, 255);
+        }
+        else {
+            _L_PWM -= map(abs(128-xAxis), 0, 128, 0, 128);
+        }
+    }
+    else if (xAxis > 128) {
+        if (isStop) {
+            _L_DIR = true;
+            _R_DIR = false;
+        }
+        else {
+            _R_PWM -= map(abs(128-xAxis), 0, 128, 0, 128);
+        }
+    }
+    else {
+        if (isStop) {
+            _L_PWM = 0;
+            _R_PWM = 0;
+        }
+    }
+}
+
+void blinker_car_control(uint8_t cl_pwm, bool cl_dir, uint8_t cr_pwm, bool cr_dir)
+{
+    digitalWrite(L_DIR_PIN, cl_dir);
+    digitalWrite(R_DIR_PIN, cr_dir);
+    analogWrite(L_PWM_PIN, cl_pwm);
+    analogWrite(R_PWM_PIN, cr_pwm);
+}
+
+void blinker_car_detect()
+{
+    if (!digitalRead(C_BAR_PIN)) {
+        if (!isWarn) {
+            Blinker.vibrate();
+            isWarn = true;
+            car_os_time = millis();
+        }
+        else if (millis() - car_os_time >= 5000) {
+            isWarn = false;
+        }
+    }
+    else {
+        isWarn = false;
+    }
+}
+
+void setup()
+{
+	Serial.begin(115200);
+
+    blinker_car_init();
+
+    Blinker.begin();
+}
+
+void loop()
+{
+	Blinker.run();
+
+    uint8_t L_PWM = 0;
+    uint8_t R_PWM = 0;
+    bool    L_DIR = true;
+    bool    R_DIR = true;
+
+    blinker_car_parse(L_PWM, L_DIR, R_PWM, R_DIR);
+
+    blinker_car_control(L_PWM, L_DIR, R_PWM, R_DIR);
+
+    blinker_car_detect();
+    
+#if defined(BLINKER_CAR_DEBUG)
+    BLINKER_LOG4("L_PWM: ", L_PWM, "  | L_DIR: ", L_DIR);
+    BLINKER_LOG4("R_PWM: ", R_PWM, "  | R_DIR: ", R_DIR);
+    Blinker.delay(2000);
+#endif
+}
